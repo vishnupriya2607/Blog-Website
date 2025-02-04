01860c7c9e1f2bb86a23bcb09761d4f6271fab54
vishnupriya2607 <125855591+vishnupriya2607@users.noreply.github.com>
2025-02-04 15:38:22 +0530

    frontend-check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'; 
+import PostList from './pages/PostList';
+import PostDetail from './pages/PostDetails';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import CategoryPost from './pages/CategoryPosts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Router>
+      <Header/>
+        <Routes>
+          <Route path='/' element ={<PostList />}/>
+        </Routes>
+      
+        <Footer/>
+      </Router>
+      
+     
     </div>
   );
 }
