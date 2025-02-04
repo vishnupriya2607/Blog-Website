772e91ee3da2dd468a7c67a1db73ece5d759dee7
vishnupriya2607 <125855591+vishnupriya2607@users.noreply.github.com>
2025-02-04 15:34:33 +0530

    backend server connection

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express =require('express');
+const mongoose =require('mongoose');
+const bodyParser =require('body-parser');
+const postRoutes =require('./routes/posts');
+const categoryRoutes =require('./routes/categories');
+const cors=require('cors');
+const app=express();
+const PORT=process.env.PORT || 8000;
+app.use(cors())
+
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/blog').then(() => console.log('Mongodb connected')).catch(err => console.log(err));
+//use router
+
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
+
+
